2694319269fb464be230cd33ec7be055b048eebf
[Francisco Barrios] <47320015+shighetari@users.noreply.github.com>
2022-06-30 16:34:23 -0400

    Forgot to post to repo

---
+++ src/app/heroes/hero.ts
@@ -0,0 +1,5 @@
+//Step 3 Creating a Hero Interface
+export interface Hero {
+    id: number;
+    name: string;
+}
---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+  //Angular only binds to public component properties.
+  //The messageService property must be public because you're going to bind to it in the template.
+  constructor(public messageService: MessageService) { }
+
+  ngOnInit(): void {
+  }
+
+}
